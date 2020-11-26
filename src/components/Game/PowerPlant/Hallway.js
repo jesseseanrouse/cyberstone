af23c1968cca9836d9acf4be2c92983c77fe6e04
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-11-25 16:26:44 -0800

    second trap is working

--- src/components/Game/PowerPlant/Hallway.js
+++ src/components/Game/PowerPlant/Hallway.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Hallway(props) {
+	const [shieldHP, setShieldHP] = React.useState(30);
 	React.useEffect(() => {
 		props.setMessage('');
 	}, []);
@@ -12,11 +13,23 @@ function Hallway(props) {
 			return (
 				<>
 					<p>
-						You walk into the hallway.{' '}
+						You walk into the hallway.
 						{props.stat.per > 4 ? (
 							<>You notice that the floor is electrified.</>
 						) : null}
 					</p>
+				</>
+			);
+		} else if (props.powerPro.trap2 === false) {
+			return (
+				<>
+					<p>The end of the hallway is blocked by an energy shield.</p>
+				</>
+			);
+		} else {
+			return (
+				<>
+					<p>You are in the hallway of the Power Plant</p>
 				</>
 			);
 		}
@@ -63,6 +76,59 @@ function Hallway(props) {
 			props.setPowerPro({ ...powerPro, trap1: true });
 		}
 	}
+	// handles second trap
+	function handleTrap2A() {
+		let newHp = props.stat.hp - 10;
+		let stat = props.stat;
+		props.setStat({ ...stat, hp: newHp });
+		let message = '';
+		if (props.stat.wil < 6) {
+			message =
+				'You reach out your hand into the shield. It burns away at your hand and you lack the will power to force yourself through';
+		} else {
+			message =
+				'You reach out your hand into the shield. It burns your hand however you can take the pain and you force your way through the shield and find the switch to turn it off.';
+			let powerPro = props.powerPro;
+			props.setPowerPro({ ...powerPro, trap2: true });
+		}
+		if (newHp < 1) {
+			message =
+				message + ' However you have taken too much damage and pass out.';
+			props.setMessage(message);
+			props.history.push(`/game/powerplant/failed`);
+		}
+		props.setMessage(message);
+	}
+	function handleTrap2B() {
+		if (props.stat.int < 7) {
+			props.setMessage('You are not smart enough to do this');
+		} else {
+			props.setMessage(
+				'You manage to overcharge the shield and it explodes. Luckily you were not harmed in the process.'
+			);
+			let powerPro = props.powerPro;
+			props.setPowerPro({ ...powerPro, trap2: true });
+		}
+	}
+	function handleTrap2C() {
+		let newShieldHP = shieldHP - props.stat.str;
+		setShieldHP(newShieldHP);
+		if (newShieldHP > 20) {
+			props.setMessage(
+				"You bash away at the shield. You don't seem to make a difference"
+			);
+		} else if (newShieldHP > 10) {
+			props.setMessage(
+				'You bash away at the shield. The shield does seem to be weakening'
+			);
+		} else if (newShieldHP > 0) {
+			props.setMessage('You bash away at the shield. It is almost failing');
+		} else if (newShieldHP < 1) {
+			props.setMessage('The shield has failed allowing you to pass.');
+			let powerPro = props.powerPro;
+			props.setPowerPro({ ...powerPro, trap2: true });
+		}
+	}
 	// First Trap
 	function Trap1() {
 		if (props.stat.per > 4) {
@@ -83,7 +149,13 @@ function Hallway(props) {
 	}
 	// Second Trap
 	function Trap2() {
-		return <p>First Trap done</p>;
+		return (
+			<>
+				<div onClick={handleTrap2A}>Try to walk through it</div>
+				<div onClick={handleTrap2B}>Try to disable it</div>
+				<div onClick={handleTrap2C}>Try to break it down</div>
+			</>
+		);
 	}
 	// actions for traps
 	function Display2() {
@@ -97,7 +169,7 @@ function Hallway(props) {
 		<>
 			{Display()}
 			{props.message}
-			{props.powerPro.trap3 ? null : <p>Actions</p>}
+			{props.powerPro.trap2 ? null : <p>Actions</p>}
 			{Display2()}
 			<p>Travel</p>
 			<Link to='/game/powerplant/lobby'>Return to Lobby</Link>
