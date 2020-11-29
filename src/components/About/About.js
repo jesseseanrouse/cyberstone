c1734b4fbdfca27de408b3c09fa6f2e9f47699b9
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-11-29 14:02:59 -0800

    added about text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,22 @@
 // Import React
 import React from 'react';
+// import css
+import './About.css'
 
 function About() {
-    return (
-       <div>
-		<h1>About</h1>
-	</div> 
-    )
-};
+	return (
+		<div className='aboutPage'>
+			<h1>Jesse Rouse</h1>
+			<img src='https://i.imgur.com/LmyXfY0.jpg?1' />
+			<div>
+				I’m a software engineer who has used my experience in education and
+				retail to pay attention to detail when troubleshooting and thinking
+				through code. When I code I set up foundations with long term goals in
+				mind and fine tune once everything is working. I wrote the front end of
+				this app using React and I used firebase as my backend.
+			</div>
+		</div>
+	);
+}
 
 export default About;
