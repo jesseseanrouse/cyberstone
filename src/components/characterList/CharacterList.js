34dd84dc8e492b161aa90628b1009325e020c784
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-11-29 10:50:44 -0800

    added delete character

--- src/components/characterList/CharacterList.js
+++ src/components/characterList/CharacterList.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useRouteMatch } from 'react-router-dom';
 // import css
-import './CharacterList.css'
+import './CharacterList.css';
 // Import Firebase
 import firebaseDb from '../Firebase/firebase';
 // import component
@@ -27,7 +27,7 @@ function CharacterList(props) {
 	// handle load
 	const handleLoad = (event) => {
 		let testName = event.target.value;
-		let key = ''
+		let key = '';
 		let ref = firebaseDb
 			.child(`users/${props.userID}/characters`)
 			.orderByChild('name')
@@ -39,15 +39,49 @@ function CharacterList(props) {
 				});
 			});
 		let ref2 = firebaseDb.once('value').then(function (snapshot) {
-			let value = snapshot.child(`users/${props.userID}/characters/${key}`).val();
-			props.setData(value)
+			let value = snapshot
+				.child(`users/${props.userID}/characters/${key}`)
+				.val();
+			props.setData(value);
 			props.history.push(`/game/scrapyard/`);
+		});
+	};
+
+	// handle delete
+	const handleDelete = (event) => {
+		let testName = event.target.value;
+		let ref = firebaseDb
+			.child(`users/${props.userID}/characters`)
+			.orderByChild('name')
+			.equalTo(testName)
+			.once('value', (snapshot) => {
+				snapshot.forEach(function (childSnapshot) {
+					let key = childSnapshot.key;
+					props.setCharID(key);
+					firebaseDb.child(`users/${props.userID}/characters/${key}`).remove();
+				});
+			});
+		// to force re-render of child
+		let ref2 = firebaseDb.once('value').then(function (snapshot) {
+			let ref3 = snapshot
+				.child(`users/${props.userID}/characters`)
+				.forEach(function (snapshot) {
+					let val = snapshot.val();
+					charList.push(val);
+				});
+			props.setList(charList);
 		});
 	};
 
 	// map charlist and render
 	const listRender = props.list.map((ele, index) => {
-		return <RenderList name={ele.name} handleLoad={handleLoad} />;
+		return (
+			<RenderList
+				name={ele.name}
+				handleLoad={handleLoad}
+				handleDelete={handleDelete}
+			/>
+		);
 	});
 
 	// Create new character
@@ -60,8 +94,16 @@ function CharacterList(props) {
 
 	return (
 		<>
-			{props.list === [] ? <p>Loading...</p> : <div className='charList'>{listRender}</div>}
-			<button className='charBut' onClick={handleClick}>Create New Character</button>
+			{props.list === [] ? (
+				<p>Loading...</p>
+			) : (
+				<>
+					<div className='charList'>{listRender}</div>
+				</>
+			)}
+			<button className='charBut' onClick={handleClick}>
+				Create New Character
+			</button>
 		</>
 	);
 }
--- src/components/characterList/RenderList.js
+++ src/components/characterList/RenderList.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 const RenderList = (props) => {
     return (
-        <>
-            <div className='charName'>{props.name}</div>
-            <button value={props.name} onClick={props.handleLoad} >Load</button>
-        </>
-    )
+			<>
+				<div className='charName'>{props.name}</div>
+				<button value={props.name} onClick={props.handleLoad}>
+					Load
+				</button>
+				<button value={props.name} onClick={props.handleDelete}>
+					Delete
+				</button>
+			</>
+		);
 }
 
 export default RenderList
