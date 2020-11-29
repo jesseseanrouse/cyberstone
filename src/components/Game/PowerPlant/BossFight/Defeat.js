fbea7d3bfe137b784ea3956c10e1e55594b356e4
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-11-29 14:59:02 -0800

    new victory and defeat scences

--- src/components/Game/PowerPlant/BossFight/Defeat.js
+++ src/components/Game/PowerPlant/BossFight/Defeat.js
@@ -9,7 +9,7 @@ function Defeat(props) {
 	}, []);
 	return (
 		<div className='gameDisplay'>
-			{props.message}
+			Dr. Crackles lashes out with a lightning bolt that strikes you right in the chest. As you lose consciousness you hear him call out, "Good Night!"
 			<div className='gameList'>
 				<Link to='/game/scrapyard/recover'>Continue</Link>
 			</div>
--- src/components/Game/PowerPlant/BossFight/Victory.js
+++ src/components/Game/PowerPlant/BossFight/Victory.js
@@ -5,11 +5,21 @@ import { Link } from 'react-router-dom';
 function Victory(props) {
 	React.useEffect(() => {
 		let powerPro = props.powerPro;
-		props.setPowerPro({ ...powerPro, boss: 2 });
+		props.setPowerPro({
+			...powerPro,
+			trap1: true,
+			trap2: true,
+			trap3: true,
+			trap4: true,
+			trap5: true,
+			trap6: true,
+			door: true,
+			boss: 2,
+		});
 	}, []);
 	return (
 		<div className='gameDisplay'>
-			{props.message}
+			You deal the final blow to Dr. Crackle and he falls to his knees. You cautiously approach him with your weapon ready. He laughs, "You won't win. I may die but he'll just find another to replace me. How do you think I got here? Ha HA HA HA..." He falls over dead with a grin on his face.
 			<div className='gameList'>
 				<Link to='/game/powerplant/generator'>Continue</Link>
 			</div>
