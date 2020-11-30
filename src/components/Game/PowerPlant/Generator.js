2b59020ac6625d2e9a96b0cacd5ffaad75e4cc27
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-11-29 20:49:38 -0800

    added action to generator room. Probably going to stop messing with this tonight.

--- src/components/Game/PowerPlant/Generator.js
+++ src/components/Game/PowerPlant/Generator.js
@@ -9,11 +9,24 @@ function Generator(props) {
 		// image is owned by ArtStation
 		document.body.style.backgroundImage =
 			"url('https://i.imgur.com/Ncr1iQk.jpg')";
-	}, []);
+    }, []);
+    function handleRecharge() {
+			if (props.stat.ep >= props.stat.epMax) {
+				props.setMessage('You are already at full energy');
+			} else {
+				let stat = props.stat;
+				props.setStat({ ...stat, ep: props.stat.epMax });
+				props.setMessage('You recharge yourself using the generator.');
+			}
+		}
 	return (
 		<div className='gameDisplay'>
 			<p>You are in the generator room</p>
 			{props.message}
+			<p>Actions</p>
+			<div className='gameList'>
+				<div onClick={handleRecharge}>Recharge</div>
+			</div>
 			<p>Travel</p>
 			<div className='gameList'>
 				<Link to='/game/powerplant/hallway'>Hallway</Link>
